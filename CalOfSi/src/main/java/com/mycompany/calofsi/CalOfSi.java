04d9f56e0479bb4ccb8fd7457e885c1ae75dacbc
vinita gurnani <vinitagurnani6@gmail.com>
2023-10-03 21:37:26 +0530

    This is my first

---
+++ CalOfSi/src/main/java/com/mycompany/calofsi/CalOfSi.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calofsi;
+
+/**
+ *
+ * @author vini
+ */
+public class CalOfSi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        float p, r, si;
+        int n;
+        p = 1000.50f; r = 15.5f; n =3;
+        si = (n*p*r)/100;
+        System.out.println(si);
+    }
+}
